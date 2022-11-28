d07570534fbdd57343be28f876e7817a0d44ce6d
Jeremy Yau <jeremyyau@me.com>
2022-11-27 17:11:40 -0800

    fix

--- apis/menu/Update.py
+++ apis/menu/Update.py
@@ -12,18 +12,19 @@ from PyQt6.QtWidgets import (
 class UpdateApp(QAction):
     def __init__(self, s):
         super().__init__("&Update App", s)
+        self.s = s
         self.triggered.connect(self.main)
 
     def main(self):
         os.system("date > server.log")
         if os.system("git pull origin master > server.log") == 0:
-            dlg = QMessageBox(self)
+            dlg = QMessageBox(self.s)
             dlg.setWindowTitle("Update App")
             dlg.setText("App Updated! Please restart App now.")
             if dlg.exec():
                 os._exit(os.EX_OK)
         else:
-            dlg = QMessageBox(self)
+            dlg = QMessageBox(self.s)
             dlg.setWindowTitle("Update App")
             dlg.setText("Error Updating. Please check server.log.")
             dlg.exec()
