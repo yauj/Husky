4790db4da39b92c7f004bb1f8956ddd7ce938868
Jeremy Yau <jeremy.yau@gpmail.org>
2023-03-10 12:11:54 -0800

    Add feedback to talkback page

--- apis/pages/pagesTalkback.py
+++ apis/pages/pagesTalkback.py
@@ -32,7 +32,9 @@ class TalkbackButton(QPushButton):
 class TalkbackWindow(QMainWindow):
     def __init__(self, config, widgets, osc):
         super().__init__()
+        self.config = config
         self.widgets = widgets
+        self.osc = osc
 
         vlayout = QVBoxLayout()
         try:
@@ -41,53 +43,58 @@ class TalkbackWindow(QMainWindow):
             vlayout.addWidget(label)
 
             self.talkbacks = {}
-            vlayout.addWidget(TalkbackAllButton(osc, self.talkbacks))
-            vlayout.addWidget(TalkbackNoneButton(osc, self.talkbacks))
 
             if "iem" not in config["osc"]:
+                if not self.osc["fohClient"].connected:
+                    raise SystemError("Not Connected to FOH Mixer")
                 if "talkbackDestination" not in config:
                     raise KeyError("talkbackDestination is not specified in config.py")
 
+                vlayout.addWidget(TalkbackAllButton(osc, self.talkbacks))
+                vlayout.addWidget(TalkbackNoneButton(osc, self.talkbacks))
+
                 command = "/config/talk/" + config["talkbackDestination"] + "/destmap"
                 self.bitmap = ["0"] * 18
-                val = osc["fohClient"].bulk_send_messages({command: None})
-                binVal = bin(val[command])[2:]
-                for idx, x in enumerate(binVal):
-                    self.bitmap[idx + (18 - len(binVal))] = x
 
                 for chName in config["personal"]:
                     if "iem_bus" in config["personal"][chName]:
+                        bus = config["personal"][chName]["iem_bus"]
+
                         hlayout = QHBoxLayout()
                         hlayout.addWidget(QLabel(chName + ":"))
                         hlayout.addWidget(TalkbackMeButton(osc, self.talkbacks, chName))
-                        self.talkbacks[chName] = TalkbackOneBox(config, osc, self.bitmap, chName, command)
+                        self.talkbacks[bus] = TalkbackOneBox(osc, self.bitmap, bus, command)
                         spacer = QWidget()
                         spacer.setFixedWidth(30)
                         hlayout.addWidget(spacer)
-                        hlayout.addWidget(self.talkbacks[chName])
+                        hlayout.addWidget(self.talkbacks[bus])
                         vlayout.addLayout(hlayout)
+                
+                self.osc["fohServer"].subscription.add(command, self.processOneSubscription)
             else:
+                if not self.osc["iemClient"].connected:
+                    raise SystemError("Not Connected to IEM Mixer")
                 if "talkbackChannel" not in config:
                     raise KeyError("talkbackChannel is not specified in config.py")
 
-                initSettings = {}
-                for chName in config["personal"]:
-                    if "iem_bus" in config["personal"][chName]:
-                        initSettings[config["talkbackChannel"] + "/mix/" + config["personal"][chName]["iem_bus"] + "/on"] = None
-                
-                    initValues = osc["iemClient"].bulk_send_messages(initSettings)
+                vlayout.addWidget(TalkbackAllButton(osc, self.talkbacks))
+                vlayout.addWidget(TalkbackNoneButton(osc, self.talkbacks))
 
                 for chName in config["personal"]:
                     if "iem_bus" in config["personal"][chName]:
+                        bus = config["personal"][chName]["iem_bus"]
+
                         hlayout = QHBoxLayout()
                         hlayout.addWidget(QLabel(chName + ":"))
                         hlayout.addWidget(TalkbackMeButton(osc, self.talkbacks, chName))
-                        self.talkbacks[chName] = TalkbackTwoBox(config, osc, initValues, chName)
+                        self.talkbacks[bus] = TalkbackTwoBox(config, osc, bus)
                         spacer = QWidget()
                         spacer.setFixedWidth(30)
                         hlayout.addWidget(spacer)
-                        hlayout.addWidget(self.talkbacks[chName])
+                        hlayout.addWidget(self.talkbacks[bus])
                         vlayout.addLayout(hlayout)
+
+                        self.osc["iemServer"].subscription.add(config["talkbackChannel"] + "/mix/" + bus + "/on", self.processTwoSubscription)
         except Exception as ex:
             logger.error(traceback.format_exc())
             label = QLabel("Error: " + str(ex))
@@ -98,19 +105,39 @@ class TalkbackWindow(QMainWindow):
         widget.setLayout(vlayout)
         self.setCentralWidget(widget)
     
+    def processOneSubscription(self, mixerName, message, arg):
+        # Update bitmap
+        binVal = bin(arg)[2:]
+        for idx, x in enumerate(binVal):
+            self.bitmap[idx + (18 - len(binVal))] = x
+
+        # Match boxes to bitmap
+        for bus in self.talkbacks:
+            self.talkbacks[bus].setChecked(self.bitmap[18 - int(bus)] == "1")
+
+    def processTwoSubscription(self, mixerName, message, arg):
+        bus = message.split("/")[4]
+        self.talkbacks[bus].setChecked(arg == 1)
+    
     def closeEvent(self, a0):
+        if "iem" not in self.config["osc"]:
+            self.osc["fohServer"].subscription.remove("/config/talk/" + self.config["talkbackDestination"] + "/destmap")
+        else:
+            for chName in self.config["personal"]:
+                if "iem_bus" in self.config["personal"][chName]:
+                    self.osc["iemServer"].subscription.remove(self.config["talkbackChannel"] + "/mix/" + self.config["personal"][chName]["iem_bus"] + "/on")
+
         del self.widgets["windows"]["Talkback"]
 
 # 1 Mixer
 class TalkbackOneBox(QCheckBox):
-    def __init__(self, config, osc, bitmap, chName, command):
+    def __init__(self, osc, bitmap, bus, command):
         super().__init__()
         self.osc = osc
         self.bitmap = bitmap
-        self.index = 18 - int(config["personal"][chName]["iem_bus"])
+        self.index = 18 - int(bus)
         self.command = command
         self.setFixedWidth(20)
-        self.setChecked(self.bitmap[self.index] == "1")
         self.stateChanged.connect(self.clicked)
     
     def clicked(self, value):
@@ -126,12 +153,11 @@ class TalkbackOneBox(QCheckBox):
 
 # 2 Mixers
 class TalkbackTwoBox(QCheckBox):
-    def __init__(self, config, osc, initValues, chName):
+    def __init__(self, config, osc, bus):
         super().__init__()
         self.osc = osc
-        self.command = config["talkbackChannel"] + "/mix/" + config["personal"][chName]["iem_bus"] + "/on"
+        self.command = config["talkbackChannel"] + "/mix/" + bus + "/on"
         self.setFixedWidth(20)
-        self.setChecked(initValues[self.command] == 1 if initValues[self.command] is not None else 1)
         self.stateChanged.connect(self.clicked)
     
     def clicked(self, value):
