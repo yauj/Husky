9c59f1d4c921e9b9aa5a6289f55932fb333f384b
Jeremy Yau <jeremy.yau@gpmail.org>
2023-03-10 11:37:22 -0800

    Add labels

--- apis/pages/pagesLayer.py
+++ apis/pages/pagesLayer.py
@@ -20,15 +20,33 @@ class PagesLayer(QWidget):
         label.setStyleSheet("font-weight: bold")
         label.setFixedHeight(20)
         vlayout.addWidget(label)
+        label = QLabel(
+            "These are pages that are top level windows.\n" +
+            "These pages have to be closed to operate the main window again.\n" +
+            "These pages have no feedback functionality.\n" +
+            "External changes will not be reflected on these pages."
+        )
+        label.setFixedHeight(65)
+        vlayout.addWidget(label)
 
         vlayout.addWidget(GainButton(config, widgets, osc))
         vlayout.addWidget(MutesButton(config, osc))
         if MIXER_TYPE == "X32":
             vlayout.addWidget(RoutingButton(config, widgets, osc))
+
             label = QLabel("Windows:")
             label.setStyleSheet("font-weight: bold")
             label.setFixedHeight(20)
             vlayout.addWidget(label)
+            label = QLabel(
+                "These are pages that are new windows.\n" +
+                "These pages can continue to be opened in the background.\n" +
+                "These pages have feedback functionality.\n" +
+                "External changes will be reflected on these pages."
+            )
+            label.setFixedHeight(65)
+            vlayout.addWidget(label)
+
             vlayout.addWidget(TalkbackButton(config, widgets, osc))
             vlayout.addWidget(AutoMixButton(config, widgets, osc))
 
